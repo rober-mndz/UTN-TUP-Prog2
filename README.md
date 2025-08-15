31befe24599fca9ca0d3c093459b408fb3ab5845
rober_mndz <robermen02@gmail.com>
2025-08-15 14:01:15 -0300

    tp1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # UTN-TUP-Prog2
 Repositorio para Trabajos practicos de la materia Programacion 2
+
+Alumno: Roberto Mendez
---
+++ TP01-Introduccion_a_Java/src/tp01/introduccion_a_java/TP01Introduccion_a_Java.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp01.introduccion_a_java;
+
+/**
+ *
+ * @author Usuario
+ */
+public class TP01Introduccion_a_Java {
+
+    public static void main(String[] args){
+        
+    }
+    
+}
