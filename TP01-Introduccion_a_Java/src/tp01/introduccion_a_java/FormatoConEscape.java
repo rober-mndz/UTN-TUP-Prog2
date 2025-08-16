a25be3a5b95461407b0630182aeca4fd71727f15
rober-mndz <robermen02@gmail.com>
2025-08-16 09:58:46 -0300

    TP1 Completo

---
+++ TP01-Introduccion_a_Java/src/tp01/introduccion_a_java/FormatoConEscape.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp01.introduccion_a_java;
+
+/**
+ *
+ * @author robermendez
+ */
+// Clase para demostrar el uso de caracteres de escape
+public class FormatoConEscape {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Juan Pérez\nEdad: 30 años\nDirección: \"Calle Falsa 123\"");
+    }
+}
